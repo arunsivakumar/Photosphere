f55a1fff4398a9b65fcfa29d4b255ee772d56458
Arun Sivakumar <arunsivakumar@live.com>
2017-07-29 11:24:07 -0600

    Add FlickrAPI URL Components

---
+++ Photosphere/FlickrAPI.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrAPI.swift
+//  Photosphere
+//
+//  Created by Lakshmi on 7/29/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import Foundation
+
+enum Method:String{
+    case interestingPhotos = "flickr.interestingness.getList"
+}
+struct FlickrAPI{
+    
+    
+}
