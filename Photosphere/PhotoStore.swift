e6ca98915b7e69321c19461c5eb9a4db1989fdf3
Arun Sivakumar <arunsivakumar@live.com>
2017-07-29 12:36:32 -0600

    Add photoStore

---
+++ Photosphere/PhotoStore.swift
@@ -0,0 +1,51 @@
+//
+//  PhotoStore.swift
+//  Photosphere
+//
+//  Created by Lakshmi on 7/29/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import Foundation
+
+
+class PhotoStore{
+
+    /// Holds a URLSession instance.
+    private let session:URLSession = {
+       let configuration = URLSessionConfiguration.default
+        return URLSession(configuration: configuration)
+    }()
+    
+    /**
+       Fetches interesting photos.
+     
+      - Connects to api.flickr.com.
+      - Uses URLSession to create URLSessionDataTask to transfer request to server.
+     
+    */
+    func fetchInterestingPhotos(){
+        
+        let url = FlickrAPI.interestingPhotosURL
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request){
+            (data, response, error) -> Void in
+            
+            if let jsonData = data{
+                
+                if let jsonString = String(data:jsonData,
+                                           encoding: .utf8){
+                    print(jsonString)
+                }
+            }else if let requestError = error{
+                print("error fetching interesting photos: \(requestError)")
+            }else{
+                print("unexpected error with request")
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+}
