624bff51acccb3fee812c32a30a928b180a2a81e
Arun Sivakumar <arunsivakumar@live.com>
2017-07-29 14:28:07 -0600

    Add NSOperation queue for data request

--- Photosphere/PhotoStore.swift
+++ Photosphere/PhotoStore.swift
@@ -65,7 +65,10 @@ class PhotoStore{
             (data, response, error) -> Void in
             
             let result = self.processPhotosRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+            
             
         }
         task.resume()
@@ -90,7 +93,9 @@ class PhotoStore{
             (data, response, error) -> Void in
             
             let result = self.processImageRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
